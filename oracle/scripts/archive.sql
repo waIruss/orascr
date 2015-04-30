61c54f8c967ce2d34acdba076bbef211d061cf10
waIruss <walrusss@gmail.com>
2015-05-01 00:14:52 +0200

    1

--- oracle/scripts/archive.sql
+++ oracle/scripts/archive.sql
@@ -1,4 +1,7 @@
-col name format a70
+alter session set NLS_DATE_FORMAT = 'mm-dd-yyyy HH24:mi:ss';
+
+col name format a65
+col first_time format a20
 select * from (select name, thread#, sequence#, first_time from v$archived_log order by 3 desc)
 where rownum <10
 /
